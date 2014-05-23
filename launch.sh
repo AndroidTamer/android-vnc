2d16d05f4d80dfbe73fcdb39e35bbb5c695ad7e8
XYSec Labs <security@xysec.com>
2014-05-23 12:20:16 +0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-Xvfb :0 -extension GLX -screen 0 480x800x24& DISPLAY=:0 emulator -avd test &
+Xvfb :0 -extension GLX -screen 0 480x800x24&
+DISPLAY=:0 emulator -avd test -snapshot -no-boot-anim&
 x11vnc -usepw -display :0
 exit 0
