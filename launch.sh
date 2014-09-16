006e0c7aadcfa6717bd8226bb1fa6790da79df78
XYSec Labs <security@xysec.com>
2014-09-17 03:57:39 +0530

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,5 +2,5 @@
 Xvfb :0 -extension GLX -screen 0 480x800x24&
 DISPLAY=:0 emulator-arm -avd test -noaudio&
 known_window=$(xwininfo -name  "5554:test"|sed -e 's/^ *//'|grep -E "^0x"|awk '{ print $1 }')
-x11vnc -usepw -id $(known_window) -display :0
+x11vnc -usepw -id $known_window -display :0
 exit 0
